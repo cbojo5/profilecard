ca6fcba64de817ef3e86dff26ca19f4cee799d02
Claudette <claudette@Claudettes-MBP.home>
2021-05-20 18:18:40 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      
+      
+       
+        <main class="leftside">
+        <h1>Learn to code by watching others</h1>
+        <p>See how experienced developers solve problems in real-time. 
+        Watching scripted tutorials is great, but understanding developers think is invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </main>
+      <div class="rightside">
+      <button class="tryitfree">Try it free 7 days <b class="tryit">then $20/mo. thereafter</b> </button>
+      </div>
+    <div class="signup">
+    <button class="firstname"> Jonathan <b class="line">|</b></button>
+    <button class="lastname">Last Name</button>
+    <button class="email">Email Address</button>
+    <button class="password">Password</button>
+    <button class="trial">Claim Your Free Trial</button>
+    <h4>By clicking the button you are agreeing to our <b class="terms">Terms and Services</b></h4>
+
+
+      
+      </div>
+    </>
   );
 }
 
