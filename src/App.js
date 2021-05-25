a08ec6ca72f68da09e247be5fd2989859759c85e
Claudette <claudette@Claudettes-MBP.home>
2021-05-24 23:08:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-
+import propic from "./images/propic2.jpeg"
 import './App.css';
+import Text from "./Component/Text"
+import React, {useState} from 'react';
 
 function App() {
 
+const [skills, setSkills] = useState("My name is Cee")
+
+const handleName =()=>{
+  setSkills("My name is Cee")
+}
+
+const handleSkills=()=>{
+  setSkills("I'm a Frontend Developer")
+}
+
+const [count, setCount] = useState(0);
+
   return (
     <>
-       
-        <main class="leftside">
-        <h1>Learn to code by watching others</h1>
-        <p>See how experienced developers solve problems in real-time. 
-        Watching scripted tutorials is great, but understanding developers think is invaluable.
-        </p>
-      </main>
-      <div class="rightside">
-      <button class="tryitfree">Try it free 7 days <b class="tryit">then $20/mo. thereafter</b> </button>
-      </div>
-    <div class="signup">
-    <button class="firstname"> Jonathan <b class="line">|</b></button>
-    <button class="lastname">Last Name</button>
-    <button class="email">Email Address</button>
-    <button class="password">Password</button>
-    <button class="trial">Claim Your Free Trial</button>
-    <h4>By clicking the button you are agreeing to our <b class="terms">Terms and Services</b></h4>
+       <section class="main">
+         <div class="card">
+           <div class="card-body"></div>
+              <div class="img-holder">
+                <img src={propic} alt="profilepic" width="200" height="210"/>
+              </div>
+              <div class="text">
+              <h1 class="greeting">
+                Hi!
+              </h1>
+              <Text tech = {skills}/>
+              </div>
+
+              <div class="container">
+                  <button class="btn" onClick ={handleName}>Name</button>
+                  <button class="btn" onClick = {handleSkills}>Skills</button>
+              </div>
+                 
+              <div class="likes">
+                <p>{count}</p>
+              <i class="fas fa-heart" onClick={()=> setCount(count + 1)}></i>
+              </div>
+              <footer class="socials">
+              <i class="fab fa-facebook-f"onClick={()=> window.open("https://facebook.com/ThePowerpuffGirls", "_blank")}></i>
+              <i class="fab fa-instagram"onClick={()=> window.open("https://instagram.com/powerpuffgirls", "_blank")}></i>
+              </footer>
+         </div>
+         
 
 
-      
-      </div>
+       </section>
+        
     </>
   );
 }
---
+++ src/Component/Text.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Text({tech}) {
+    return (
+
+        <h3>{tech}</h3>
+    )
+}
